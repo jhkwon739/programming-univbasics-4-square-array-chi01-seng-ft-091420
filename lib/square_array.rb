df677f6e39579921d4ea46eb14937905ca001fe4
Jasmine Kwon <jhkwon1472@gmail.com>
2020-08-13 02:36:03 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   # your code here
   square_array = []
   array.length.times { 
-    |i| square_array.push(array[i]**2) 
+    |index| square_array.push(array[index]**2) 
     
   }
   square_array
